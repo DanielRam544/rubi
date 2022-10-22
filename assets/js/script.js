7d00ca97d56d71bd53f26f95d0e4d799620c1c29
git-trevor <daniel.guerrero@outlook.com>
2022-10-21 23:51:11 -0500

    Ajustes en mapa
    Se logro poner el mapa a un lado

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,25 +21,21 @@ function muestraSucursal() {
     nuevoDiv.className = "card bg-light mb-3";
     nuevoDiv.style = "max-width: 25rem; float:left;";
 	nuevoDiv.innerHTML =
-        '<div class="card-header">Joyería Rubí</div>' +
-        '   <div class="card-body">' +
-        '       <p class="card-text">Tienda: Zona centro</p>' +
-        '       <p class="card-text">Dirección: Calle Guanajuato #3 bis, Jerez, Zacatecas</p>' +
-        '       <p class="card-text">CP: 99300</p>' +
-        '       <p class="card-text">Teléfono: 494-100-0057</p>' +
-        '   </div>' +
-        '</div>' +
-        '<div class="card bg-light mb-3">' +
-        '   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3682.0920996043524!2d-102.99211588255615!3d22.650354500000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8682effd818b017b%3A0x868d8ce2f0cf4e0f!2sJOYER%C3%8DA%20RUBI!5e0!3m2!1ses!2smx!4v1666292493242!5m2!1ses!2smx" width="100%" height="600" style="border:transparent;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        '<div class="card-header"><i class="bx bxl-sketch" style="color:#ad292e"></i> Joyería Rubí <i class="bx bxl-sketch" style="color:#ad292e"></i></div>' +
+        '<div class="card-body">' +
+            '<p class="card-text">Tienda: Zona centro</p>' +
+            '<p class="card-text">Dirección: Calle Guanajuato #3 bis, Jerez, Zacatecas</p>' +
+            '<p class="card-text">CP: 99300</p>' +
+            '<p class="card-text">Teléfono: 494-100-0057</p>' +
         '</div>';
     formulario.appendChild(nuevoDiv);
 
-    // nuevoFrame.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3682.0920996043524!2d-102.99211588255615!3d22.650354500000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8682effd818b017b%3A0x868d8ce2f0cf4e0f!2sJOYER%C3%8DA%20RUBI!5e0!3m2!1ses!2smx!4v1666292493242!5m2!1ses!2smx";
-    // nuevoFrame.width = 700;
-    // nuevoFrame.height = 550;
-    // nuevoFrame.style = "border:0; padding-bottom: 10px;";
-    // nuevoFrame.allowFullscreen = "";
-    // nuevoFrame.loading = "lazy";
-    // nuevoFrame.referrerPolicy = "no-referrer-when-downgrade";
-    // formulario.appendChild(nuevoFrame);
+    nuevoFrame.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3682.0920996043524!2d-102.99211588255615!3d22.650354500000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8682effd818b017b%3A0x868d8ce2f0cf4e0f!2sJOYER%C3%8DA%20RUBI!5e0!3m2!1ses!2smx!4v1666292493242!5m2!1ses!2smx";
+    nuevoFrame.width = 900;
+    nuevoFrame.height = 650;
+    nuevoFrame.style = "border:ridge;";
+    nuevoFrame.allowFullscreen = "";
+    nuevoFrame.loading = "lazy";
+    nuevoFrame.referrerPolicy = "no-referrer-when-downgrade";
+    formulario.appendChild(nuevoFrame);
 }
